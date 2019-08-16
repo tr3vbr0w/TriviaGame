0ef7bc386a4424234e3a21b546f3be47196efa38
tbsteeze <trevbrow@live.com>
2019-08-15 21:38:42 -0600

    broke up into more sensible chunks with andi

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,7 +26,7 @@ var question = [
     ques : "What is the most consumed beer in Utah?",
     incorrectAns : ["Budweiser", "Uinta Lime Pilsner", "PBR Tallboys"],
     correctAns : "Diet Coke"
-    //correctAns : incorrectAns[3]
+    // correctAns : incorrectAns[3]
     },
 ]
 // console.log(question[4].correctAns)
@@ -36,11 +36,10 @@ var usedQuestions = [];
 var correct = 0;
 var incorrect = 0;
 var selected
-
 //Function to pull random question from unusedQuestion array, add it to the usedQuestion array, and save the correct answer within the question div
 function randomQuestion() {
-    var q = Math.floor(Math.random() * Math.floor(question.length))
-    selected = question[q];
+    var q = Math.floor(Math.random() * Math.floor(unusedQuestions.length))
+    selected = unusedQuestion[q];
     usedQuestions.splice(1, 0, selected);
     unusedQuestions.splice(selected, 1);
     return selected
@@ -63,30 +62,44 @@ function displayQuestion() {
   // $('<div>').addClass('answer-display');
 }
 
+var gameTimer = setTimeout(function(){
+  //#start-game button is not hiding here once created and clicked
+  // setInterval()
+
+  // When timer hits 0, I want to clear the question display
+
+  //Once the display is cleared, i want to let the user know that time has expired
+
+  //Once the display is cleared, I want to create a button to move on to the next question
+
+
+},2000) 
+
+function buttonCreator() {
+  $('.question-display').html("");
+  $('.question-display').append('<h1>').text('time is up. Click button for next question');
+  $('.question-display').append($('<button>').text('nextquestion').attr('id', 'start-game'));
+}
+
 
 
 
 //On key press, the browser selects a random question object from the questions array then fills the information in the cooresponding 
-$(document).on('click', '#start-game', function() {
+
+// This Func start my game, calls my func that gen questions and start my timer. 
+$('#start-game').on('click', function() {
   $('#start-game').hide();
+
+  // When a user clicks the start game btn I want to select a question randomly
   randomQuestion();
+  // When a user clicks the start game btn I want to display that random question
   displayQuestion();
+  // When this btn is clicked I want to start my timer because that is what I will use to track the game play
+  gameTimer();
+
   //Begin timeout function, which runs for 10 seconds then clears the question-display div, displaying time is up
-  setTimeout (function(){
-    $('.question-display').html("");
-    $('.question-display').append('<h1>').text('time is up. Click button for next question');
-    //#start-game button is not hiding here once created and clicked
-    $('.question-display').append($('<button>').text('nextquestion').attr('id', 'start-game'));
-
-  },1000) 
-  
-
-  
-
-  
-  
-  
 })
+// $('')
 console.log(usedQuestions)
 
 
